a67659a461a66c599b268a68c0c91310a6547313
Le Nhat <ltn2412@gmail.com>
2026-07-14 10:46:48 +0700

    Update

--- scratch.js
+++ scratch.js
@@ -5,14 +5,8 @@ function updateHtml(filePath) {
   let html = fs.readFileSync(filePath, "utf8");
   
   // Revert general label and value flex
-  html = html.replace(/flex: 0 0 35%;/g, "flex: 0 0 38%;");
-  html = html.replace(/flex: 0 0 65%;/g, "flex: 0 0 62%;");
-  
-  // Make the first row specific
-  const targetRow = '<div class="label">Mã đơn hàng /<br/>Order No:</div>\\s*<div class="value">{{ORDER_NO}}</div>';
-  const newRow = '<div class="label" style="flex: 0 0 33%;">Mã đơn hàng /<br/>Order No:</div>\n      <div class="value" style="flex: 0 0 67%;">{{ORDER_NO}}</div>';
-  
-  html = html.replace(new RegExp(targetRow, 'g'), newRow);
+  html = html.replace(/flex: 0 0 38%;/g, "flex: 0 0 42%;");
+  html = html.replace(/flex: 0 0 62%;/g, "flex: 0 0 58%;");
   
   fs.writeFileSync(filePath, html);
 }
